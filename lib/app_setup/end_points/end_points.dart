771254b7bb1c7084a3b134b75e54fb449f3b00b0
Prabesh Rai <raiprabesh789@gmail.com>
2021-10-15 22:41:16 +0545

    endpoints

---
+++ lib/app_setup/end_points/end_points.dart
@@ -0,0 +1,4 @@
+class Endpoints {
+  static const String loginEndpoint = 'user/login';
+  static const String registerEndpoint = 'user/register';
+}
---
+++ lib/app_setup/end_points/service_handler.dart
@@ -0,0 +1,3 @@
+class ServiceHandler {
+  static const String baseUrl = 'http://10.0.2.2/better-buys/api/';
+}
